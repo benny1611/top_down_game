2f583fa059389d493b85c52a8107b3abd70bf734
Beniamin Bratulescu <beniamin.bratulescu@tum.de>
2022-10-02 08:33:55 +0200

    some more playable version

--- src/levels/Level.gd
+++ src/levels/Level.gd
@@ -7,12 +7,14 @@ onready var dialogueBox = get_node("Polygon2D/RichTextLabel")
 func _ready():
 	get_node("Player").visible = false
 	get_node("Player/AnimatedSprite").visible = false
+	get_node("Player/Fog").visible = false
 	cutscene.play("move_to_right")
 	cutscene.connect("animation_finished", self, "ready_for_level")
 	get_node("Player").movement = false
 
 func ready_for_level(value: String):
 	get_node("Player/AnimatedSprite").visible = true
+	get_node("Player/Fog").visible = true
 	get_node("Player").visible = true
 	get_node("Player/Camera2D").current = true
 	get_node("Player").movement = true
